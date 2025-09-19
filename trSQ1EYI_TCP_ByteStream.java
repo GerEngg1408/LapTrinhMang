5b136ab944f2ddd17c336ccf1e1a9dca5193f339
GerEngg1408 <144197923+GerEngg1408@users.noreply.github.com>
2025-09-19 09:40:51 +0700

    Ok

--- trSQ1EYI_TCP_ByteStream.java
+++ trSQ1EYI_TCP_ByteStream.java
@@ -1,7 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+[Mã câu hỏi (qCode): trSQ1EYI].  Một chương trình server hỗ trợ kết nối qua giao thức TCP tại cổng 2206 (hỗ trợ thời gian giao tiếp tối đa cho mỗi yêu cầu là 5s). 
+Yêu cầu xây dựng chương trình client thực hiện kết nối tới server trên sử dụng luồng byte dữ liệu (InputStream/OutputStream) để trao đổi thông tin theo thứ tự: 
+a.	Gửi mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode". Ví dụ: "B16DCCN999;C64967DD"
+b.	Nhận dữ liệu từ server là một chuỗi gồm các giá trị nguyên được phân tách với nhau bằng  "|"
+Ex: 2|5|9|11
+c.	Thực hiện tìm giá trị tổng của các số nguyên trong chuỗi và gửi lên server
+Ex: 27
+d.	Đóng kết nối và kết thúc
  */
 package lap.trinh.mang;
 
@@ -67,5 +72,6 @@ public class trSQ1EYI_TCP_ByteStream {
 	}
 
 }
+
 
 
