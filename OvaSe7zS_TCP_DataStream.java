b99f5751cd22968831e865266026fe2088d222ab
GerEngg1408 <144197923+GerEngg1408@users.noreply.github.com>
2025-09-19 09:36:50 +0700

    Ok

--- OvaSe7zS_TCP_DataStream.java
+++ OvaSe7zS_TCP_DataStream.java
@@ -1,7 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+[Mã câu hỏi (qCode): OvaSe7zS].  Một chương trình server cho phép kết nối qua TCP tại cổng 2207 (hỗ trợ thời gian liên lạc tối đa cho mỗi yêu cầu là 5 giây). Yêu cầu xây dựng chương trình client thực hiện giao tiếp với server sử dụng luồng data (DataInputStream/DataOutputStream) để trao đổi thông tin theo thứ tự:
+a. Gửi mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode".
+Ví dụ: "B10DCCN003;C6D7E8F9"
+b. Nhận lần lượt:
+•	Một số nguyên k là độ dài đoạn.
+•	Chuỗi chứa mảng số nguyên, các phần tử được phân tách bởi dấu phẩy ",".
+Ví dụ: Nhận k = 3 và "1,2,3,4,5,6,7,8".
+c. Thực hiện chia mảng thành các đoạn có độ dài k và đảo ngược mỗi đoạn, sau đó gửi mảng đã xử lý lên server. Ví dụ: Với k = 3 và mảng "1,2,3,4,5,6,7,8", kết quả là "3,2,1,6,5,4,8,7". Gửi chuỗi kết quả "3,2,1,6,5,4,8,7" lên server.
+d. Đóng kết nối và kết thúc chương trình
  */
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -74,3 +80,4 @@ public class OvaSe7zS_TCP_DataStream {
         }
     }  
 }
+
