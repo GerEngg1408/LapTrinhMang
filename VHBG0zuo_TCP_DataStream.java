d8b9387a5ad3e8aab87d8172f92834786784c7c4
GerEngg1408 <144197923+GerEngg1408@users.noreply.github.com>
2025-09-19 09:38:22 +0700

    Ok

--- VHBG0zuo_TCP_DataStream.java
+++ VHBG0zuo_TCP_DataStream.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+[Mã câu hỏi (qCode): VHBG0zuo].  Một chương trình server cho phép kết nối qua TCP tại cổng 2207 (hỗ trợ thời gian liên lạc tối đa cho mỗi yêu cầu là 5 giây). Yêu cầu xây dựng chương trình client thực hiện giao tiếp với server sử dụng luồng data (DataInputStream/DataOutputStream) để trao đổi thông tin theo thứ tự:
+a. Gửi mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode".
+Ví dụ: "B10DCCN001;A1B2C3D4"
+b. Nhận một số nguyên hệ thập phân từ server. Ví dụ: 255
+c. Chuyển đổi số nguyên nhận được sang hai hệ cơ số 8 và 16. Gửi lần lượt chuỗi kết quả lên server. Ví dụ: Với số 255 hệ thập phân, kết quả gửi lên sẽ là hai chuỗi "377" và "FF".
+d. Đóng kết nối và kết thúc chương trình.
  */
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -51,3 +54,4 @@ public class VHBG0zuo_TCP_DataStream {
         }
     }
 }
+
