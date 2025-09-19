931434a0acaf72a1c071a40e8688371f9ba66b58
GerEngg1408 <144197923+GerEngg1408@users.noreply.github.com>
2025-09-19 09:39:13 +0700

    not Ok

--- bmB0Ipfy_TCP_ByteStream.java
+++ bmB0Ipfy_TCP_ByteStream.java
@@ -1,7 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+[Mã câu hỏi (qCode): bmB0Ipfy].  Một chương trình server hỗ trợ kết nối qua giao thức TCP tại cổng 2206 (hỗ trợ thời gian giao tiếp tối đa cho mỗi yêu cầu là 5s). Yêu cầu xây dựng chương trình client thực hiện kết nối tới server sử dụng luồng byte dữ liệu (InputStream/OutputStream) để trao đổi thông tin theo thứ tự:
+a. Gửi mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode".
+Ví dụ: "B16DCCN999;C89DAB45"
+b. Nhận dữ liệu từ server là một chuỗi các số nguyên được phân tách bởi ký tự ",".
+Ví dụ: "8,4,2,10,5,6,1,3"
+c. Tính tổng của tất cả các số nguyên tố trong chuỗi và gửi kết quả lên server.
+Ví dụ: Với dãy "8,4,2,10,5,6,1,3", các số nguyên tố là 2, 5, 3, tổng là 10. Gửi lên server chuỗi "10".
+d. Đóng kết nối và kết thúc chương trình.
  */
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -95,4 +100,5 @@ public class bmB0Ipfy_TCP_ByteStream {
         return true;
     }
 }
+
 
