5e27e378a903b71d011327162b0a99234bda111b
GerEngg1408 <144197923+GerEngg1408@users.noreply.github.com>
2025-09-19 09:53:52 +0700

    Ok

--- OlBvkw2C_TCP_CharacterStream.java
+++ OlBvkw2C_TCP_CharacterStream.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+[Mã câu hỏi (qCode): 0lBvkw2C].  Một chương trình server cho phép kết nối qua giao thức TCP tại cổng 2208 (hỗ trợ thời gian giao tiếp tối đa cho mỗi yêu cầu là 5 giây). Yêu cầu là xây dựng một chương trình client tương tác với server sử dụng các luồng ký tự (BufferedReader/BufferedWriter) theo kịch bản sau:
+a. Gửi một chuỗi chứa mã sinh viên và mã câu hỏi với định dạng "studentCode;qCode". Ví dụ: "B15DCCN999;C1234567"
+b. Nhận từ server một chuỗi chứa nhiều từ, các từ được phân tách bởi khoảng trắng. Ví dụ: "hello world this is a test example"
+c. Sắp xếp các từ trong chuỗi theo độ dài, thứ tự xuất hiện. Gửi danh sách các từ theo từng nhóm về server theo định dạng: "a, is, this, test, hello, world, example".
+d. Đóng kết nối và kết thúc chương trình.
  */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -62,3 +64,4 @@ public static void main(String[] args) {
         }
     }    
 }
+
